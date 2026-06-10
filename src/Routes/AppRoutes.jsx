b2e819415273f37a9085430db5c80da6cbc1e471
Ivan <160286233+ivanzhsds@users.noreply.github.com>
2026-06-10 20:51:33 +0000

    confirma

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from "react-router-dom"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
+import Home from "../pages/home"
+import Login from "../pages/login"
 
 export default function AppRoutes(){
     return(

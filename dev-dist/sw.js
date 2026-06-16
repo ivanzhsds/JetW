9678f1f123daae74f3aa07ba296aca7b7d10d3e2
Ivan <160286233+ivanzhsds@users.noreply.github.com>
2026-06-16 21:30:12 +0000

    confirma saiiiiiiiiii

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.79mkm2cu6r"
+    "revision": "0.l5ngvn6obkc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
